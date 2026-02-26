0c22c81996c34fc4bf2497d4c55f1c33fb3d65a9
Emil Brandon Kunju Herrera <buyme3409@gmail.com>
2026-02-26 12:06:06 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+function Experience() {
+  return (
+    <>
+      <h3>Software Developer</h3>
+      <h4>Sigma Lab Sdn Bhd</h4>
+    </>
+  )
+
+}
+
+function App() {
+  return (
+    <>
+      <h1> Brandon Herrera</h1>
+      <h2>Software developer</h2>
+      <Experience />
+    </>
+  )
+}
+
+export default App;
